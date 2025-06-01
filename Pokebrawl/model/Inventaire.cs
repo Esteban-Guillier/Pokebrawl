831db3aef685676b2a42e91650a3d960218036ce
esteban <outil.info.este@gmail.com>
2025-06-01 14:55:46 +0200

    Avancer gestion magasin

---
+++ Pokebrawl/model/Inventaire.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokebrawl.model
+{
+    public class Inventaire
+    {
+        public Dictionary<string, int> ObjetsPossedes { get; set; } = new();
+
+        public void Ajouter(string nomObjet, int quantite = 1)
+        {
+            if (ObjetsPossedes.ContainsKey(nomObjet))
+                ObjetsPossedes[nomObjet] += quantite;
+            else
+                ObjetsPossedes[nomObjet] = quantite;
+        }
+
+        public bool Possede(string nomObjet) => ObjetsPossedes.ContainsKey(nomObjet) && ObjetsPossedes[nomObjet] > 0;
+        public bool Retirer(string nomObjet, int quantite = 1)
+        {
+            if (!Possede(nomObjet)) return false;
+            ObjetsPossedes[nomObjet] -= quantite;
+            if (ObjetsPossedes[nomObjet] <= 0)
+                ObjetsPossedes.Remove(nomObjet);
+            return true;
+        }
+    }
+}
--- Pokebrawl/view/Magasin.xaml.cs
+++ Pokebrawl/view/Magasin.xaml.cs
@@ -18,19 +18,73 @@ namespace Pokebrawl.view
     /// <summary>
     /// Logique d'interaction pour Magasin.xaml
     /// </summary>
-    public partial class Magasin : Window
+    public partial class PageMagasin : Page
     {
+        private Joueur _joueur;
         private Frame _mainFrame;
-        private GameSession _session;
-        public Magasin(Frame mainFrame, GameSession session)
+        public List<MagasinItem> Items = new()
+        {
+            new MagasinItem { Nom = "Potion", Image = "/view/image/objets/potion.png", Prix = 200, Type = ItemType.Potion },
+            new MagasinItem { Nom = "Ball", Image = "/view/image/objets/ball.png", Prix = 150, Type = ItemType.Ball }
+        };
+
+        public PageMagasin(Frame mainFrame, Joueur joueur)
         {
             InitializeComponent();
             _mainFrame = mainFrame;
-            _session = session;
+            _joueur = joueur;
+            ShopItemsList.ItemsSource = Items;
+            UpdateArgent();
         }
-        public Magasin()
+
+        private void UpdateArgent()
         {
-            InitializeComponent();
+            ArgentText.Text = $"Argent : {_joueur.Argent} ₽";
+        }
+
+        private void Acheter_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            var item = btn.Tag as MagasinItem;
+            if (item == null) return;
+            if (_joueur.Argent < item.Prix)
+            {
+                MessageBox.Show("Pas assez d'argent !");
+                return;
+            }
+
+            // Achat d'une Potion => choisir le Pokémon à soigner
+            if (item.Type == ItemType.Potion)
+            {
+                var choixPokemonWindow = new ChoixPokemonWindow(_joueur.Equipe.Pokemons);
+                if (choixPokemonWindow.ShowDialog() == true)
+                {
+                    var pokemon = choixPokemonWindow.PokemonSelectionne;
+                    if (pokemon != null)
+                    {
+                        // Soigner le Pokémon (exemple : PV max)
+                        pokemon.PV = pokemon.PVMax;
+                        _joueur.Argent -= item.Prix;
+                        _joueur.Inventaire.Ajouter(item.Nom, 1);
+                        UpdateArgent();
+                        MessageBox.Show($"{pokemon.Nom} est soigné !");
+                    }
+                }
+            }
+            // Achat d'une Ball
+            else if (item.Type == ItemType.Ball)
+            {
+                _joueur.Argent -= item.Prix;
+                _joueur.Inventaire.Ajouter(item.Nom, 1);
+                UpdateArgent();
+                MessageBox.Show("Ball ajoutée à l'inventaire !");
+            }
+        }
+
+        private void Continuer_Click(object sender, RoutedEventArgs e)
+        {
+            // On reprend la série de combats là où on en était
+            _mainFrame.Navigate(new PageCombat(_mainFrame, _joueur));
         }
     }
 }
